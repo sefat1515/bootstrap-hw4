945a75de8def075c8b0d20b043a6279179179966
sefat1515 <91536653+sefat1515@users.noreply.github.com>
2021-11-16 17:17:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+var exampleEl = document.getElementById('example')
+var tooltip = new bootstrap.Tooltip(exampleEl, options)
